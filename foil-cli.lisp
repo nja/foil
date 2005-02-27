d256c38a5c38ceff022f3d291376d92953a6f20b
rhickey <rhickey>
2005-02-27 18:26:48 +0000

    simplified to use get-library-classnames

--- foil-cli.lisp
+++ foil-cli.lisp
@@ -1,55 +1,12 @@
 (use-package :foil)
-(def-foil-class "System.Object")
-(def-foil-class "System.Reflection.Assembly")
-(def-foil-class "System.Reflection.Module")
-(def-foil-class "System.Type")
-(def-foil-class "System.Collections.IEnumerator")
-(def-foil-class "System.Collections.IEnumerable")
 
-(use-package "System")
-(use-package "System.Reflection")
-(use-package "System.Collections")
-
-(defmacro doenum ((e enum) &body body)
-  (let ((genum (gensym)))
-    `(let ((,genum ,enum))
-       (do ()
-           ((not (ienumerator.movenext ,genum)))
-         (let ((,e (ienumerator.current ,genum)))
-           ,@body)))))
-
-(defun get-assembly-classnames (assembly-file-name &rest packages)
-  "returns a list of strings, assemblies should be of the form \"system/io\"
-  for recursive lookup and \"system/io/\" for non-recursive"
-  (let* ((asm (assembly.loadfrom assembly-file-name))
-         (types (ienumerable.GetEnumerator (assembly.GetTypes asm)))
-         (dot-packages (mapcar (lambda (p)
-                                 (substitute #\. #\/ p)) packages))
-         (names ()))
-    (doenum (e types)
-      (let ((ename (|System|::Type.tostring e)))
-          ;(format t "~A~%" ename)
-        (flet ((matches (package)
-                 (and (eql 0 (search package ename))
-                      (or (not (eql #\. (schar package (1- (length package))))) ;recursive
-                          (not (find #\. ename :start (length package))))))) ;non-subdirectory
-          (when  (and ;don't grab implementation details classes
-                  (not (or (find #\$ ename)
-                           (find #\+ ename)
-                           (search "__" ename)
-                           (search "PrivateImplementationDetails" ename)))
-                  (some #'matches dot-packages))
-            (push ename names)))))
-    names))
-
-
-(defun gen-wrappers ()
-  (dump-wrapper-defs-to-file "/foil/cli-system-collections.lisp" (get-assembly-classnames "/WINDOWS/Microsoft.NET/Framework/v1.1.4322/mscorlib.dll" "System.Collections"))
-  (dump-wrapper-defs-to-file "/foil/cli-system.lisp" (get-assembly-classnames "/WINDOWS/Microsoft.NET/Framework/v1.1.4322/mscorlib.dll" "System/"))
-  (compile-file "/foil/cli-system-collections.lisp")
-  (compile-file "/foil/cli-system.lisp")
-)
+;this is just an example of some wrapper gen invocations, do similar stuff for your system
+(defun gen-cli-wrappers ()
+  (compile-file
+   (dump-wrapper-defs-to-file "/foil/cli-system-collections.lisp"
+    (get-library-classnames
+     "/WINDOWS/Microsoft.NET/Framework/v1.1.4322/mscorlib.dll" "System.Collections")))
+  (compile-file
+   (dump-wrapper-defs-to-file "/foil/cli-system.lisp"
+    (get-library-classnames "/WINDOWS/Microsoft.NET/Framework/v1.1.4322/mscorlib.dll" "System/"))))
  
-;(gen-wrappers) 
-
-
--- foil-java.lisp
+++ foil-java.lisp
@@ -1,68 +1,34 @@
-;(load "/dev/foil/foil")
 (use-package :foil)
-(def-foil-class "java.util.jar.JarFile")
-(def-foil-class "java.util.Enumeration")
-(def-foil-class "java.util.zip.ZipEntry")
-
-(use-package "java.util")
-(use-package "java.util.jar")
-(use-package "java.util.zip")
-
-(defmacro doenum ((e enum) &body body)
-  (let ((genum (gensym)))
-    `(let ((,genum ,enum))
-       (do ()
-           ((not (enumeration.hasmoreelements ,genum)))
-         (let ((,e (enumeration.nextelement ,genum)))
-           ,@body)))))
-
-(defun get-jar-classnames (jar-file-name &rest packages)
-  "returns a list of strings, packages should be of the form \"java/lang\"
-  for recursive lookup and \"java/util/\" for non-recursive"
-  (let* ((jar (jarfile.new jar-file-name))
-         (entries (jarfile.entries jar))
-         (names ()))
-    (doenum (e entries)
-      (unless (zipentry.isdirectory e)
-        (let ((ename (zipentry.getname e)))
-          (flet ((matches (package)
-                   (and (eql 0 (search package ename))
-                        (or (not (eql #\/ (schar package (1- (length package))))) ;recursive
-                            (not (find #\/ ename :start (length package))))))) ;non-subdirectory
-            (when (and (eql (search ".class" ename)
-                            (- (length ename) 6)) ;classname
-                       ;don't grab anonymous inner classes
-                       (not (and (find #\$ ename)
-                                 (digit-char-p (schar ename (1+ (position #\$ ename))))))
-                       (some #'matches packages))
-              (push (nsubstitute #\. #\/ (subseq ename 0 (- (length ename) 6)))
-                    names))))))
-    names))
-
 
 ;this is just an example of some wrapper gen invocations, do similar stuff for your system
-(defun gen-wrappers ()
-;/System/Library/Frameworks/JavaVM.framework/Versions/1.4.2/Classes/classes.jar
-  #+nil(dump-wrapper-defs-to-file "~/foil/swt.lisp" (get-jar-classnames "/Users/rich/downloads/eclipse/plugins/org.eclipse.swt.carbon_3.0.0/ws/carbon/swt.jar" "org/eclipse/swt/" "org/eclipse/swt/program/" "org/eclipse/swt/printing/" "org/eclipse/swt/layout/" "org/eclipse/swt/dnd/" "org/eclipse/swt/graphics/" "org/eclipse/swt/custom/" "org/eclipse/swt/events/" "org/eclipse/swt/printing/" "org/eclipse/swt/widgets/" "org/eclipse/swt/browser/" "org/eclipse/swt/awt/"))
+(defun gen-java-wrappers ()
+  (compile-file
+   (dump-wrapper-defs-to-file "/foil/java-lang.lisp"
+                              (get-library-classnames "/j2sdk1.4.2/jre/lib/rt.jar" "java/lang/")))
+  (compile-file
+   (dump-wrapper-defs-to-file "/foil/java-io.lisp"
+                              (get-library-classnames "/j2sdk1.4.2/jre/lib/rt.jar" "java/io/")))
+  (compile-file
+   (dump-wrapper-defs-to-file "/foil/java-util.lisp"
+                              (get-library-classnames "/j2sdk1.4.2/jre/lib/rt.jar" "java/util/")))
 
-  (dump-wrapper-defs-to-file "/foil/java-lang.lisp" (get-jar-classnames "/j2sdk1.4.2/jre/lib/rt.jar" "java/lang/"))
-  (dump-wrapper-defs-to-file "/foil/java-util.lisp" (get-jar-classnames "/j2sdk1.4.2/jre/lib/rt.jar" "java/util/"))
-  (dump-wrapper-defs-to-file "/foil/java-io.lisp" (get-jar-classnames "/j2sdk1.4.2/jre/lib/rt.jar" "java/io/"))
-  (dump-wrapper-defs-to-file "/foil/java-sql.lisp" (get-jar-classnames "/j2sdk1.4.2/jre/lib/rt.jar" "java/sql/"))
-  (dump-wrapper-defs-to-file "/foil/javax-sql.lisp" (get-jar-classnames "/j2sdk1.4.2/jre/lib/rt.jar" "javax/sql/"))
-  (dump-wrapper-defs-to-file "/foil/swt.lisp" (get-jar-classnames "/dev/eclipse/plugins/org.eclipse.swt.win32_3.0.1/ws/win32/swt.jar" "org/eclipse/swt/" "org/eclipse/swt/program/" "org/eclipse/swt/printing/" "org/eclipse/swt/layout/" "org/eclipse/swt/dnd/" "org/eclipse/swt/graphics/" "org/eclipse/swt/custom/" "org/eclipse/swt/events/" "org/eclipse/swt/printing/" "org/eclipse/swt/widgets/" "org/eclipse/swt/browser/" "org/eclipse/swt/awt/"))
+  (compile-file
+   (dump-wrapper-defs-to-file "/foil/java-sql.lisp"
+                              (get-library-classnames "/j2sdk1.4.2/jre/lib/rt.jar" "java/sql/")))
 
-  (dump-wrapper-defs-to-file "/foil/java-lang.lisp" (get-jar-classnames "/j2sdk1.4.2/jre/lib/rt.jar" "java/lang/"))
-
-  (dump-wrapper-defs-to-file "/foil/commons-httpclient.lisp" (get-jar-classnames "/dev/commons-httpclient-2.0.2/commons-httpclient-2.0.2.jar" "org/apache/commons/httpclient"))
-
-
-
-  (compile-file "/foil/java-lang")
-  (compile-file "/foil/java-util")
-  (compile-file "/foil/java-io")
-  (compile-file "/foil/java-sql")
-  (compile-file "/foil/javax-sql")
-  (compile-file "/foil/javax-servlet")
-  (compile-file "/foil/commons-httpclient")
-  (compile-file "/foil/swt"))
+  ;sometimes LW 4.4 dies after successfully generating this fasl - hmmm...
+  (compile-file
+   (dump-wrapper-defs-to-file "/foil/swt.lisp"
+     (get-library-classnames "/eclipse/plugins/org.eclipse.swt.win32_3.0.1/ws/win32/swt.jar"
+                         "org/eclipse/swt/"
+                         "org/eclipse/swt/program/" "org/eclipse/swt/printing/"
+                         "org/eclipse/swt/layout/" "org/eclipse/swt/dnd/"
+                         "org/eclipse/swt/graphics/" "org/eclipse/swt/custom/"
+                         "org/eclipse/swt/events/" "org/eclipse/swt/printing/"
+                         "org/eclipse/swt/widgets/" "org/eclipse/swt/browser/"
+                         "org/eclipse/swt/awt/")))
+  
+  (compile-file
+   (dump-wrapper-defs-to-file "/foil/commons-httpclient.lisp"
+     (get-library-classnames
+      "/dev/commons-httpclient-2.0.2/commons-httpclient-2.0.2.jar" "org/apache/commons/httpclient"))))
